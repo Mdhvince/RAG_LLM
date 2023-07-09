c28c05a2cba988fed15b5c88c74e418a1360b606
mdhvince <vinceslas.medhy@gmail.com>
2023-07-09 16:28:31 +0200

    change policy signature

--- rlhf/policy_net.py
+++ rlhf/policy_net.py
@@ -1,31 +1,20 @@
 import torch
 import torch.nn as nn
-from peft import PeftModel
-
-from utils import load_model
 
 
 class PolicyNet(nn.Module):
-    def __init__(self, original_model, generation_config, peft_dir="peft-checkpoint-local") -> None:
+    def __init__(self, peft_model, generation_config) -> None:
         """
         The policy network is an LLMPeftModel that outputs the ids of the tokens generated.
 
-        :param original_model: The original model (non-finetuned)
-        :param peft_dir: The directory of the saved PEFT model
+        :param peft_model: The peft_model model (finetuned)
         :param generation_config: The generation config used for the PEFT model
         """
         super(PolicyNet, self).__init__()
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.gen_config = generation_config
-
-        self.model = PeftModel.from_pretrained(
-            original_model,
-            peft_dir,
-            torch_dtype=torch.bfloat16,
-            device_map="auto",
-            is_trainable=True
-        )
+        self.model = peft_model
 
     def _format(self, input_ids):
         """
